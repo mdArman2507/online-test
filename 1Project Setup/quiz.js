971b4f8af357187a1147dc42952b2a24725d68a9
mdArman2507 <mdarman2507@gmail.com>
2023-09-24 12:56:36 +0530

    create options

--- 1Project Setup/quiz.js
+++ 1Project Setup/quiz.js
@@ -3,10 +3,18 @@ const questionObj =
       category: 'Food & Drink',
       id: 'qa-1',
       correctAnswer: 'Three',
-      answers: ['Two', 'Three ', 'Four', 'Five'],
+      options: ['Two', 'Three ', 'Four', 'Five'],
       question:
         "How many pieces of bun are in a Mcdonald's Big Mac?",
     };
-    const{correctAnswer,answers,question}=questionObj;
-    const questionEl=document.getElementById("question");
+    const{correctAnswer,options,question}=questionObj;
+    const questionEl=document.getElementById('question');
+    const optionsEl=document.getElementById('options');
+    const scoreEl=document.getElementById('score');
     questionEl.textContent=question;
+    
+    options.forEach((opt) => {
+      const btn=document.createElement('button');
+      btn.textContent=opt;
+      optionsEl.appendChild(btn);
+    });
